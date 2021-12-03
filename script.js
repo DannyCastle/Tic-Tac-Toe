abf19545d831faa65a9b774cdfaa0f32bf9d9fb0
Daniel Castillo <danny.castle@hotmail.com>
2021-12-02 21:32:59 -0500

    pseudocode planning

---
+++ script.js
@@ -0,0 +1,21 @@
+const player = (piece) => {
+    return {piece};
+    //creates a player.piece that will equal x or o
+};
+
+const gameBoard = ((index) => {
+    /* uses index to change array of gameboard
+       passes new array to display controller */
+})();
+
+const displayController = ((board) => {
+    /*take board array and use it to change displayed board */
+
+})();
+
+const fieldParser = ((button) => {
+    /* listens to field buttons
+       identifies which one hit
+       sends index to gameBoard
+       */
+})();
