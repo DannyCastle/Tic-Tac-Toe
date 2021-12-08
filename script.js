74f226e8402131cc59dd699e2604adacb0ec4b07
Daniel Castillo <danny.castle@hotmail.com>
2021-12-07 20:27:52 -0500

    found way to mark buttons

--- script.js
+++ script.js
@@ -1,21 +1,64 @@
-const player = (piece) => {
-    return {piece};
+const player = (marker) => {
+    return {marker};
     //creates a player.piece that will equal x or o
 };
 
-const gameBoard = ((index) => {
-    /* uses index to change array of gameboard
-       passes new array to display controller */
+const gameBoard = (() => {
+    let board = new Array(9);
+
+    const update = (index, marker) => {
+        board[index] = marker;
+        displayController.update();
+    };
+
+    const show = () => console.log(board);
+
+    return {
+        board,
+        update,
+        show,
+    };
+
 })();
 
-const displayController = ((board) => {
+
+const displayController = (() => {
     /*take board array and use it to change displayed board */
 
+
+    const update = () => {
+        for (let i = 0; i < 9; ++i){
+            field.buttons[i].textContent = gameBoard.board[i];
+        }
+    };
+
+    return {
+        update,
+    };
 })();
 
-const fieldParser = ((button) => {
+const field = (() => {
     /* listens to field buttons
        identifies which one hit
        sends index to gameBoard
        */
+    let index = '';
+    const buttons = document.querySelectorAll("#gameboard-ctn button");
+    let parse = buttons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            let marker = 'x';
+            index = button.dataset.location;
+            gameBoard.update(index, marker);
+        });
+    });
+    return {
+        buttons,
+    };
 })();
+
+function updateBoard (index) {
+    /* uses index to change array of gameboard
+       passes new array to display controller */
+    console.log('this index is in the gamboard ' + index);
+
+};
