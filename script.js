d9fecf46409a274c5c2862d779c605f62f02640c
Daniel Castillo <danny.castle@hotmail.com>
2021-12-14 22:29:42 -0500

    added winning condition and clearing of board

--- script.js
+++ script.js
@@ -6,7 +6,21 @@ const player = (marker) => {
 const gameBoard = (() => {
     let board = new Array(9);
 
+    const restartButton = document.querySelector('#restart-btn');
+    restartButton.addEventListener('click', (e) => clear());
+    const clear = () => {
+        for (let i = 0; i < 9; ++i){
+            board[i] = null;
+        };
+        displayController.update();
+    };
+
     const update = (index, marker) => {
+        while (board[index] != null){
+            console.log(board[index]);
+            game.currentPlayer();
+            return;
+        };
         board[index] = marker;
         displayController.update();
     };
@@ -46,9 +60,8 @@ const field = (() => {
     const buttons = document.querySelectorAll("#gameboard-ctn button");
     let parse = buttons.forEach((button) => {
         button.addEventListener('click', (e) => {
-            let marker = 'x';
             index = button.dataset.location;
-            gameBoard.update(index, marker);
+            gameBoard.update(index, game.currentPlayer());
         });
     });
     return {
@@ -56,9 +69,62 @@ const field = (() => {
     };
 })();
 
-function updateBoard (index) {
-    /* uses index to change array of gameboard
-       passes new array to display controller */
-    console.log('this index is in the gamboard ' + index);
 
-};
+const game = (() => {
+    const player1 = player('X');
+    const player2 = player('O');
+
+    let lastMove = 'O';
+
+    let currentPlayer = () => {
+        if (lastMove === 'O'){
+            lastMove = 'X';
+            return player1.marker;
+        };
+        lastMove = 'O';
+        return player2.marker;
+    };
+
+    const checkForWin = () => {
+        const winningArrays = [
+        '012',
+        '345',
+        '678',
+        '036',
+        '147',
+        '258',
+        '048',
+        '246'
+        ];
+
+        const getPlayerIndices = (mark) => {
+            let indices = '';
+            for (i = 0; i < gameBoard.board.length; ++i){
+                if (gameBoard.board[i] === mark){
+                    indices += i;
+                };
+            };
+            return indices;
+        };
+
+        let player1Arr = getPlayerIndices(player1.marker);
+        let player2Arr = getPlayerIndices(player2.marker);
+
+        let winner = 'No One';
+
+        winningArrays.forEach((array) => {
+            if (player1Arr.includes(array)) winner = player1.marker;
+            else if (player2Arr.includes(array)) winner = player2.marker;
+        });
+
+        console.log(`The Winner is ${winner}!`);
+
+    };
+
+
+    return{
+        checkForWin,
+        currentPlayer,
+    };
+
+})();
